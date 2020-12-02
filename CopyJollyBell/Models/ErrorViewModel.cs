e79bb3af6ac6199038ca3bbdb3c2ab244234da2a
Betsq <betsq9@gmail.com>
2020-12-02 23:13:21 +0200

    Добавьте файлы проекта.

---
+++ CopyJollyBell/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CopyJollyBell.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
