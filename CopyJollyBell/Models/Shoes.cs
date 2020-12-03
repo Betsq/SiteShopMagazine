0d39ad457c7f1346759323b5cb7595b0d832f79b
Betsq <betsq9@gmail.com>
2020-12-03 11:55:19 +0200

    -Create Model file -Create and initialization Database

---
+++ CopyJollyBell/Models/Shoes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CopyJollyBell.Models
+{
+    public class Shoes
+    {
+        public string Name { get; set; }
+    }
+}
